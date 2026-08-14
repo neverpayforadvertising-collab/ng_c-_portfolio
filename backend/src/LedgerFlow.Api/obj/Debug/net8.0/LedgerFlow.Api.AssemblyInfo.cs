ffe6f8031235e7d530de43f7558d7dffdea5c2f2
meritdev <neverpayforadvertising@gmail.com>
2026-08-14 21:04:19 +0300

    backend req dto

--- backend/src/LedgerFlow.Api/obj/Debug/net8.0/LedgerFlow.Api.AssemblyInfo.cs
+++ backend/src/LedgerFlow.Api/obj/Debug/net8.0/LedgerFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LedgerFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55bd893a2ecdfe70328b05b155fab33fb99bf74d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20152705120e5bffcd806cc47a9fe8fa52dfea8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LedgerFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LedgerFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
