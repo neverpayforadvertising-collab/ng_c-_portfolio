9202cffd883a2acd96cc7df7f5e1480f4f8e0154
meritdev <neverpayforadvertising@gmail.com>
2026-08-14 18:03:41 +0300

    add: app component

--- frontend/ledgerflow-web/src/app/app.routes.ts
+++ frontend/ledgerflow-web/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'customers',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'customers',
+    loadComponent: () =>
+      import(
+        './features/customers/pages/customer-list/customer-list.component'
+      ).then(
+        module => module.CustomerListComponent
+      )
+  },
+
+  {
+    path: '**',
+    redirectTo: 'customers'
+  }
+];
