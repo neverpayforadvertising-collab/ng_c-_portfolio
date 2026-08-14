ed877dbaa013560e6be38d05930b61057bef6983
meritdev <neverpayforadvertising@gmail.com>
2026-08-14 21:49:25 +0300

    generate the add customer dialog

---
+++ frontend/ledgerflow-web/src/app/features/customers/components/customer-form-dialog/customer-form-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-form-dialog',
+  imports: [],
+  templateUrl: './customer-form-dialog.html',
+  styleUrl: './customer-form-dialog.scss',
+})
+export class CustomerFormDialog {}
